13f281a07bcd3672b8d1ec5c0161beb747ccd2ff
RubenMiramontes <ruben.miramontes@hotmail.com>
2022-12-06 14:15:13 -0600

    Cambios con breakpoints  en publicidad y publicacion

---
+++ js/petff.js
@@ -0,0 +1,18 @@
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
