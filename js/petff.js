1a44b6147b61343682de801f9df636de6601f957
megumivelez <rebecavelez.r@gmail.com>
2022-12-07 18:42:43 -0600

    se hacen ajustes al responsive de la pagina, se agrega la barra de navegacion(footer), se hacen ajustes al carrusel, se ajusta el encabezado y se le agrega menu hamburguesa

--- js/petff.js
+++ js/petff.js
@@ -1,13 +1,11 @@
-function showPerfil(identifier){
-    console.log("showPerfil");
-
+function showPerfil(identifier) {
     let perfiles = document.querySelectorAll(".perfil-mascota.active");
 
     perfiles.forEach((perfil) => {
         perfil.classList.remove('active');
     });
 
-    let el = document.getElementById(identifier);
-    console.log("element: ", el);
+    let el = document.getElementById('mascota-' + identifier);
+
     el.classList.add("active");
 }
