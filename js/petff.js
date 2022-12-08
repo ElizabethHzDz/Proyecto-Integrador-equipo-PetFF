107ec6e6bee38648c88f0d3c88c968d4aa9c4e77
RubenMiramontes <ruben.miramontes@hotmail.com>
2022-12-08 03:19:01 -0600

    Comentarios en HTML agregados, Preview y Publicacion de imagen con DOM

--- js/petff.js
+++ js/petff.js
@@ -1,18 +1,51 @@
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+const imgInput = document.querySelector("#imageInput");
+var uploadedImg = "";
 
-        reader.onload = function (e) {
-            $('#imageResult')
-                .attr('src', e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
+imgInput.addEventListener("change", function(){
+    var checkImg = document.getElementsByClassName("loadedImg");
+    if(checkImg.length > 0 ){
+        document.querySelectorAll(".loadedImg").forEach(checkImg => checkImg.remove());
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            uploadedImg = reader.result;
+            let image = document.createElement('img');
+            let image2 = document.createElement('img');
+            image.src = uploadedImg;
+            image2.src = uploadedImg;
+            image.className = "loadedImg";
+            image.id = "imgForUse";
+            image2.className = "loadedImg";
+            document.getElementById("displayImage").appendChild(image);
+            document.getElementById("displayImage2").appendChild(image2);
+        })
+        reader.readAsDataURL(this.files[0]);
+    }else {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploadedImg = reader.result;
+        let image = document.createElement('img');
+        let image2 = document.createElement('img');
+        image.src = uploadedImg;
+        image2.src = uploadedImg;
+        image.className = "loadedImg";
+        image.id = "imgForUse";
+        image2.className = "loadedImg";
+        document.getElementById("displayImage").appendChild(image);
+        document.getElementById("displayImage2").appendChild(image2);
+    })
+    reader.readAsDataURL(this.files[0]);
 }
+})
 
-$(function () {
-    $('#upload').on('change', function () {
-        readURL(input);
-    });
-});
+const imgPublish = document.querySelector("#pubButton");
+
+imgPublish.addEventListener("click", function(){
+    let imgPub = document.createElement('img');
+    imgPub.src = document.getElementById("imgForUse").src;
+    document.getElementById("imgPublish").appendChild(imgPub);
+    var checkImg = document.getElementsByClassName("loadedImg");
+    if(checkImg.length > 0 ){
+        document.querySelectorAll(".loadedImg").forEach(checkImg => checkImg.remove());
+    }
+})
