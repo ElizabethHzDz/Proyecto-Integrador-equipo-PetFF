a5e9f34ac567b184e50c5f2cd220545db4269c61
Ismael Herrera <ismaelherrera97@yahoo.es>
2022-12-20 10:55:08 -0600

    Se agrega API de Maps; dos HTMLs extra para registro diferenciado

--- js/formularioRegistro.js
+++ js/formularioRegistro.js
@@ -4,6 +4,11 @@ var cedula = document.querySelector('#espacioCedula');
 var botonUser = document.querySelector("#user");
 var inputUser = document.querySelector('#espacioUser');
 
+var botonPet = document.querySelector("#pet");
+var inputPet = document.querySelector('#espacioPet');
+
+
+
 botonUser.addEventListener("click", function (){
     var inputsUser = `<div class="form-group">
     <form id="form">
@@ -24,6 +29,10 @@ botonUser.addEventListener("click", function (){
                                 <label for="password">Contraseña</label>
                                 <input type="password" class="form-control" id="password" placeholder="Crea una contraseña">
                             </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlFile1">Añade una foto de perfil</label>
+                                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                            </div>
                             <div class="col-auto">
                                 <label class="sr-only" for="username">Nombre de usuario/a</label>
                                 <div class="input-group mb-2">
@@ -33,12 +42,13 @@ botonUser.addEventListener("click", function (){
                                     <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Elige el nombre para tu cuenta" required>
                                 </div>
                             </div>
+
                             <div class="form-check d-flex justify-content-center p-1 m-0">
                                 <input type="checkbox" class="form-check-input p-1 m-0" required>
                                 <label class="form-check-label p-1" for="exampleCheck1"><a href="" target="_blank">  Términos y condiciones de PetFF</a></label>
                               </div>
                               <br>
-                            <button type="submit" class="btn" id="boton">Enviar</button>
+                            <button href="registroMascota.html" type="button" class="btn" id="boton">Continuar</button>
                         </form>
     
     </div>
@@ -63,6 +73,11 @@ botonCedula.addEventListener("click", function () {
                             <label for="cedula">Cédula profesional</label>
                             <input type="text" class="form-control" id="cedulaProf" placeholder="Introduce tu cédula" required>
                             </div>
+                            </div>
+                            <div class="form-group">
+                            <label for="direccionConsultorio">Dirección consultorio</label>
+                            <input type="text" class="form-control" id="direccionC" placeholder="Indica a tus pacientes dónde pueden encontrarte" required>
+                            </div>
                             <div class="form-group">
                                 <label for="email">Correo electrónico</label>
                                 <input type="email" class="form-control" id="email" placeholder="correo@ejemplo.com" required>
@@ -70,6 +85,11 @@ botonCedula.addEventListener("click", function () {
                             <div class="form-group col-md-12">
                                 <label for="password">Contraseña</label>
                                 <input type="password" class="form-control" id="password" placeholder="Crea una contraseña">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleFormControlFile1">Añade una foto de perfil</label>
+                                <input type="file" class="form-control-file" id="exampleFormControlFile1">
                             </div>
                             <div class="col-auto">
                                 <label class="sr-only" for="username">Nombre de usuario/a</label>
@@ -87,10 +107,10 @@ botonCedula.addEventListener("click", function () {
                               <br>
                             <button type="submit" class="btn" id="boton">Enviar</button>
                         </form>
-
     `;
     cedula.innerHTML += inputCelula;
 
     agregarCedula();
 })
+
 
