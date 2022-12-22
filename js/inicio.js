2cb30589357a429c971efca04be2d9b48d2f20d9
RubenMiramontes <115729856+RubenMiramontes@users.noreply.github.com>
2022-12-22 16:20:25 -0600

    Update inicio.js
    Se agrega un punto en todos los src

--- js/inicio.js
+++ js/inicio.js
@@ -66,9 +66,9 @@ buttonAdd.addEventListener("click", function() {
         <div class="row">
           <!-- Donde irá la imagen del usuario y nombres de mascota y dueño -->
           <div class="d-flex col-9 justify-content-left align-items-center" id="perfil">
-            <img class="img-thumbnail" id="perfilMascota" src="/assetsInicio/marliese-streefland-2l0CWTpcChI-unsplash 1.png"
+            <img class="img-thumbnail" id="perfilMascota" src="./assetsInicio/marliese-streefland-2l0CWTpcChI-unsplash 1.png"
               alt="Foto de perfil mascota">
-            <img id="perfilHumano" src="/assetsInicio/harps-joseph-tAvpDE7fXgY-unsplash 1.png" alt="Foto de perfil humano">
+            <img id="perfilHumano" src="./assetsInicio/harps-joseph-tAvpDE7fXgY-unsplash 1.png" alt="Foto de perfil humano">
             <div class="container d-flex flex-column names">
               <a href="https://getbootstrap.com/" class="mascota" target="_blank">Max el Beagle</a>
               <p class="humano">José Sánchez</p>
@@ -89,16 +89,16 @@ buttonAdd.addEventListener("click", function() {
         </div>
         <!-- Espacio para agregar imagen de publicación -->
         <div class="row p-0 m-0" id="">
-        <img src="/assetsInicio/anthony-fomin-xNOslF_987U-unsplash.jpg" alt="Imagen de Publicacion">
+        <img src="./assetsInicio/anthony-fomin-xNOslF_987U-unsplash.jpg" alt="Imagen de Publicacion">
         </div>
         <!-- Fila para botones de like y comentarios -->
         <div class="row">
           <div class="col-1 like">
-            <img class="buttons" src="/assetsInicio/iconos/Like.png" alt="Like">
+            <img class="buttons" src="./assetsInicio/iconos/Like.png" alt="Like">
           </div>
           <p class="col-2 mt-3 text-left">54</p>
           <div class="col-1 comment">
-            <img class="buttons" src="/assetsInicio/iconos/Comment.png" alt="Comment">
+            <img class="buttons" src="./assetsInicio/iconos/Comment.png" alt="Comment">
           </div>
           <p class="col-2 mt-3 text-left">9</p>
           <div class="col-6"></div>
